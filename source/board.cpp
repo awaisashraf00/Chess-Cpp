8d35d2037ba50411d9923eb0c64ce92c2df77dc3
awaisashraf00 <ashrafawais915@gmail.com>
2026-02-02 12:37:16 +0500

    Board Desgined

--- source/board.cpp
+++ source/board.cpp
@@ -11,6 +11,13 @@ void Board::Display_Board() {
         for (int j = 0; j < rows; j++) {
             DrawRectangle(cell_size * j, cell_size * i, cell_size, cell_size,
                           ((i + j) % 2 == 0) ? SKIN : BROWN);
-        }
+            int piece = grid[i][j];
+            if (piece != 0 && pieces.find(piece) != pieces.end()) {
+                Texture2D texture = LoadTexture(pieces[piece].c_str());
+                DrawTextureEx(texture, {(float)(cell_size * j), (float)(cell_size * i)}, 0.0f, 
+                              (float)cell_size / texture.width, WHITE);
+                UnloadTexture(texture);
+            }
+        }    
     }
 }
--- source/board.h
+++ source/board.h
@@ -19,18 +19,18 @@ class Board
                         {2,3,4,5,6,4,3,2}};
 
         std::map<int,std::string> pieces = {
-                            {-1, "pawn"}
-                            ,{-2, "rook"}
-                            ,{-3, "knight"}
-                            ,{-4, "bishop"}
-                            ,{-5, "queen"}
-                            ,{-6, "king"}
-                            ,{1, "pawn"}
-                            ,{2, "rook"}
-                            ,{3, "knight"}
-                            ,{4, "bishop"}
-                            ,{5, "queen"}
-                            ,{6, "king"}};
+                            {-1, "/Users/sherazrajput/Chess/game/pieces-basic-png/black-pawn.png"}
+                            ,{-2, "/Users/sherazrajput/Chess/game/pieces-basic-png/black-rook.png"}
+                            ,{-3, "/Users/sherazrajput/Chess/game/pieces-basic-png/black-knight.png"}
+                            ,{-4, "/Users/sherazrajput/Chess/game/pieces-basic-png/black-bishop.png"}
+                            ,{-5, "/Users/sherazrajput/Chess/game/pieces-basic-png/black-queen.png"}
+                            ,{-6, "/Users/sherazrajput/Chess/game/pieces-basic-png/black-king.png"}
+                            ,{1, "/Users/sherazrajput/Chess/game/pieces-basic-png/white-pawn.png"}
+                            ,{2, "/Users/sherazrajput/Chess/game/pieces-basic-png/white-rook.png"}
+                            ,{3, "/Users/sherazrajput/Chess/game/pieces-basic-png/white-knight.png"}
+                            ,{4, "/Users/sherazrajput/Chess/game/pieces-basic-png/white-bishop.png"}
+                            ,{5, "/Users/sherazrajput/Chess/game/pieces-basic-png/white-queen.png"}
+                            ,{6, "/Users/sherazrajput/Chess/game/pieces-basic-png/white-king.png"}};
     public:
         Board();
         void Display_Board();        
